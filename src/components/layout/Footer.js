792bf58a72c3e7daf579561536930ebdfeb72ade
Miguel Ortiz <m.ortiz.11@gmail.com>
2025-05-30 14:08:23 -0600

    Add first structure

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>© {new Date().getFullYear()} Miguel Ortiz. All rights reserved.</p>
+    </footer>
+  )
+}
+export default Footer;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Menu</a></li>
+        <li><a href="/contact">Reservations</a></li>
+        <li><a href="/contact">Order Online</a></li>
+        <li><a href="/contact">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+export default Navbar;
